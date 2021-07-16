16b618e4e7643f4374c6398424285587b8645dd0
Ivaylo Iliev <43753631+iiliev-nemetschek@users.noreply.github.com>
2021-07-16 14:35:35 +0300

    Add missing swift-tools-version

--- Package.swift
+++ Package.swift
@@ -22,6 +22,37 @@
 //  THE SOFTWARE.
 //
 
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(name: "PromisedFuture", dependencies : [], exclude: ["Tests"])
+let package = Package(
+    name: "PromisedFuture",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "PromisedFuture",
+            targets: ["PromisedFuture"]),
+    ],
+    dependencies: [],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "PromisedFuture",
+            dependencies: [],
+            path: "Source",
+            exclude: [
+            "Info.plist"
+            ]),
+        .testTarget(
+            name: "PromisedFutureTests",
+            dependencies: ["PromisedFuture"],
+            path: "Tests",
+            exclude: [
+            "Info.plist",
+            "CheckCocoaPodsQualityIndexes.rb"
+            ]),
+    ]
+)
